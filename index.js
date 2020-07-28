dab820a5c50ebcdf48f5ae2a47e9b24a6e86b874
Azim Javed <thatazimjaved@gmail.com>
2020-07-28 18:13:27 +0530

    Setup new endpoints

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const http = require('http')
 const WebSocket = require('ws')
+const bodyParser = require('body-parser')
+const crypto = require('crypto-js')
+
+const connection = require('./lib/database/mongo')
+const { Appliance } = require('./lib/database/models/appliances')
+const { Auth } = require('./lib/database/models/auth')
+
+const { firebaseAuth } = require('./lib/firebase')
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 const server = http.createServer(app);
 
@@ -13,7 +24,7 @@ wss.on('connection', (ws) => {
     ws.on('message', (message) => {
 
         ws.send(`{"type":"OTHER", "data": "Hello, you sent -> ${message}"}`);
-    }); 
+    });
     ws.send('{"type":"OTHER", "data":"Hi there, I am a WebSocket server"}');
 });
 
@@ -21,18 +32,65 @@ wss.on('connection', (ws) => {
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/pages/index.html')
 })
-app.get('/off', (req, res) => {
-    wss.clients.forEach(client => {
-        client.send(`{"type":"OFF"}`);
-    })
-    res.redirect('/')
+
+app.post('/login', (req, res) => {
+    let time = Date()
+    firebaseAuth.signInWithEmailAndPassword(req.body.email, req.body.password)
+        .then(() => {
+            let token = crypto.MD5('/AzIm/' + req.body.email + '*' + req.body.password + '*' + time.toString() + '/AyEsha/')
+            let authToken = new Auth({ token: token, date: time, valid: true })
+            authToken.save()
+                .then(savedToken => {
+                    console.log("New login detected")
+                    res.json({ authenticated: true, token: token, valid })
+                })
+                .catch((err) => {
+                    console.log("Database save failed")
+                    res.json({ authenticated: false, token: null })
+                })
+        })
+        .catch(() => {
+            console.log("Sign in failed")
+            res.json({ authenticated: false, token: null })
+        })
 })
 
-app.get('/on', (req, res) => {
-    wss.clients.forEach(client => {
-        client.send(`{"type":"ON"}`);
+app.post('/appliance', (req, res) => {
+    Appliance.find({}, (err, result) => {
+        if (err) {
+            res.json({ type: "error", data: [] })
+        } else {
+            res.json({ type: "success", data: result })
+        }
     })
-    res.redirect('/')
+})
+
+app.post('/applianceToggle', (req, res) => {
+    Auth.find({ token: req.body.token }, (err, res) => {
+        if (err) {
+            res.json({ status: "Auth Failed" })
+        } else if (res.length > 0) {
+            if (res[0].valid) {
+                if (wss.clients.length > 0) {
+                    wss.clients.forEach(client => {
+                        client.send(`{
+                            "type":"${req.body.payload.action}", 
+                            "id":"${req.body.payload.appliance.id}",
+                            "pin": "${req.body.appliance.pin}"
+                        }`)
+                    })
+                    res.json({ status: "SUCCESS" })
+                } else {
+                    res.json({ status: "No client found" })
+                }
+
+            } else {
+                res.json({ status: "Auth Failed" })
+            }
+        } else {
+            res.json({ status: "Auth Failed" })
+        }
+    })
 })
 
 app.get('/ping', (req, res) => {
